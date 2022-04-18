7b74e062b5f2e7fd4c95a10677371d758d44d8cd
Linda <lwoo@plaid.com>
2022-04-18 13:12:30 -0700

    fix account type

--- client/src/components/types.ts
+++ client/src/components/types.ts
@@ -42,42 +42,13 @@ export interface AccountType {
   user_id: number;
   plaid_account_id: string;
   name: string;
-  mask: string;
-  official_name: string;
-  current_balance: number;
-  available_balance: number;
-  iso_currency_code: string;
-  unofficial_currency_code: string;
-  ach_account: string;
-  ach_routing: string;
-  ach_wire_routing: string;
-  owner_names: string[];
-  emails: string[];
-  processor_token: string;
-  cust_url: string;
-  funding_source_url: string;
-  number_of_transfers: number;
-  type: 'depository' | 'investment' | 'loan' | 'credit';
-  subtype:
-    | 'checking'
-    | 'savings'
-    | 'cd'
-    | 'money market'
-    | 'ira'
-    | '401k'
-    | 'student'
-    | 'mortgage'
-    | 'credit card';
+  subtype: 'checking' | 'savings';
   created_at: string;
   updated_at: string;
 }
 export interface UserType {
   id: number;
   username: string | null;
-  fullname: string | null;
-  email: string | null;
-  identity_check: boolean;
-  should_verify_identity: boolean;
   created_at: string;
   updated_at: string;
 }
