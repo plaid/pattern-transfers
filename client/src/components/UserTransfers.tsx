311cb65d2da113cd33d5b99f9fbfc6d6ab358c18
Linda <lwoo@plaid.com>
2022-04-22 09:32:45 -0700

    fix classnames

--- client/src/components/UserTransfers.tsx
+++ client/src/components/UserTransfers.tsx
@@ -12,13 +12,13 @@ const UserTransfers: React.FC<Props> = (props: Props) => {
       ? null
       : props.transfers.map((transfer, index) => {
           return (
-            <div className="transfers_table_row">
+            <div className="transfers-table__row">
               {' '}
-              <div className=" transfers_table_data column1">{index + 1}</div>
-              <div className=" transfers_table_data column2">
+              <div className=" transfers-table__data column1">{index + 1}</div>
+              <div className=" transfers-table__data column2">
                 {transfer.status}
               </div>
-              <div className=" transfers_table_data column3">
+              <div className=" transfers-table__data column3">
                 ${transfer.amount.toFixed(2)}
               </div>
             </div>
@@ -29,11 +29,11 @@ const UserTransfers: React.FC<Props> = (props: Props) => {
     <div>
       {' '}
       <h4>Payment History</h4>
-      <div className="transfers_table">
-        <div className="transfers_table_row">
-          <div className="transfers_table_header column1">Month</div>
-          <div className="transfers_table_header column2">Status</div>
-          <div className="transfers_table_header column3">Amount</div>
+      <div className="transfers-table">
+        <div className="transfers-table__row">
+          <div className="transfers-table__header column1">Month</div>
+          <div className="transfers-table__header column2">Status</div>
+          <div className="transfers-table__header column3">Amount</div>
         </div>
         {tableRows}
       </div>
