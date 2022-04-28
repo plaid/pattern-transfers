712251d8673be7bc402dabf64c1b5249e6c5d1f5
Linda <lwoo@plaid.com>
2022-04-28 07:56:28 -0700

    add link customization change

--- server/routes/linkTokens.js
+++ server/routes/linkTokens.js
@@ -59,7 +59,7 @@ router.post(
         transfer: {
           intent_id: transferIntentId,
         },
-        link_customization_name: LINK_CUSTOMIZATION_NAME,
+        link_customization_name: LINK_CUSTOMIZATION_NAME || 'transfer_ui_woo',
       };
 
       // If user has entered a redirect uri in the .env file
