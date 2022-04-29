d89110c4d6794dffadc7fc55ac7c4b4c2979c316
Linda <lwoo@plaid.com>
2022-04-29 11:10:55 -0700

    add comments

--- server/routes/appStatus.js
+++ server/routes/appStatus.js
@@ -5,8 +5,7 @@
 const express = require('express');
 const {
   createInitialStatus,
-  setNumberOfEvents,
-  updateAppAccountBalance,
+  retrieveEvents,
   retrieveAppStatus,
 } = require('../db/queries');
 const { asyncWrapper } = require('../middleware');
@@ -25,29 +24,38 @@ router.get(
   asyncWrapper(async (req, res) => {
     try {
       const status = await retrieveAppStatus();
+      let lastEventId = 0;
 
       if (status.length === 0) {
-        // get all events associated with clientId in order to get the last event_id
-        let afterId = 0;
-        let events = [];
-        let eventsToFetch = true;
-        const count = 25;
-        while (eventsToFetch) {
-          //fetch the events
-          const sycnRequest = {
-            after_id: afterId,
-            count: count,
-          };
-          const response = await plaid.transferEventSync(sycnRequest);
-          events = [...events, ...response.data.transfer_events];
-          if (response.data.transfer_events.length === count) {
-            afterId += count;
-          } else {
-            eventsToFetch = false;
+        // if no app status, create it
+        const allEvents = await retrieveEvents();
+        if (allEvents.length > 0) {
+          // get last event id and save it to database
+          lastEventId = allEvents[allEvents.length - 1].event_id;
+        } else {
+          // if no events in database, get all events associated with clientId in order to get the last event_id
+          let afterId = 0;
+          let events = [];
+          let eventsToFetch = true;
+          const count = 25;
+          while (eventsToFetch) {
+            //fetch the events
+            const sycnRequest = {
+              after_id: afterId,
+              count: count,
+            };
+            const response = await plaid.transferEventSync(sycnRequest);
+            events = [...events, ...response.data.transfer_events];
+            if (response.data.transfer_events.length === count) {
+              afterId += count;
+            } else {
+              eventsToFetch = false;
+            }
+            events.sort((a, b) => (a.event_id > b.event_id ? -1 : 1));
           }
-          events.sort((a, b) => (a.event_id > b.event_id ? -1 : 1));
+          lastEventId = events[0].event_id;
         }
-        await createInitialStatus(events[0].event_id);
+        await createInitialStatus(lastEventId);
       }
 
       res.json(status);
